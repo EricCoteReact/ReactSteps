7b7604288374e91e11ede9d70e0607394b7bac3e
Eric Cote <eric@coteexpert.com>
2018-11-13 17:24:24 -0500

    Component class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//We can use the new arrow functions
-const Hello = ({firstName, lastName}) => (
-        <h1>
-            Hello {firstName} {lastName}!
-        </h1>
-    );
+//We can use classes we gain then a few things:
+//state and lifecycle methods
+class Hello extends React.Component{
+    render() {
+        return ( 
+            <h1>
+                Hello {this.props.firstName} {this.props.lastName}!
+            </h1>
+        );
+    }
+
+}
+
 
 ReactDOM.render(<Hello firstName="John" lastName="Smith" />, 
                 document.getElementById('root'));
