0cfe19edce0eceb8a62482c7ad851f3fe190b5a6
Eric Cote <eric@coteexpert.com>
2018-11-13 16:15:13 -0500

    Round brackets around multiline json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//We can use multiple props
+//We need round backets in the following syntax
 function Hello(props) {
-    return <h1>Hello {props.children} {props.lastName}</h1>
+    return (
+        <h1>
+            Hello {props.children} {props.lastName}
+        </h1>
+    );
 } 
 
-ReactDOM.render(<Hello lastName="Smith">John</Hello>, document.getElementById('root'));
+ReactDOM.render(<Hello lastName="Smith">John</Hello>, 
+                document.getElementById('root'));
 
