a444529dc2b7adda6bf8a24be70c442a91484299
Eric Cote <eric@coteexpert.com>
2018-11-13 16:22:32 -0500

    arrow functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//We can use destructuring instead of using props
-function Hello({firstName, lastName}) {
-    return (
+//We can use the new arrow functions
+const Hello = ({firstName, lastName}) => (
         <h1>
             Hello {firstName} {lastName}!
         </h1>
     );
-} 
 
 ReactDOM.render(<Hello firstName="John" lastName="Smith" />, 
                 document.getElementById('root'));
