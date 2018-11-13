63eb8fd5afa131188578c50539b98117b3d9915f
Eric Cote <eric@coteexpert.com>
2018-11-13 16:12:21 -0500

    multiple props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//props can be used to pass data
+//We can use multiple props
 function Hello(props) {
-    return <h1>Hello {props.children}</h1>
+    return <h1>Hello {props.children} {props.lastName}</h1>
 } 
 
-
-ReactDOM.render(<Hello>Eric</Hello>, document.getElementById('root'));
+ReactDOM.render(<Hello lastName="Smith">John</Hello>, document.getElementById('root'));
 
