12b0a890cb0ca15d80b5fd127629eff19f5d1cc6
Eric Cote <eric@coteexpert.com>
2018-11-13 15:36:42 -0500

    Reactify page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-document.getElementById('root').innerHTML="<h1>Hello world</h1>";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(React.createElement("h1", null, "Hello world!!!"), 
+                document.getElementById('root'));
+
