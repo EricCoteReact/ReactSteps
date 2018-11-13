7a970bd80757fae39cbdf6248d8f0a7a736af475
Eric Cote <eric@coteexpert.com>
2018-11-13 15:41:01 -0500

    JSX in a var

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//This syntax is called JSX
-ReactDOM.render(<h1>Hello world!</h1>, 
-                document.getElementById('root'));
+//We can put the jsx in a variable
+const hello = <h1>Hello world!</h1>
+
+
+ReactDOM.render(hello, document.getElementById('root'));
 
