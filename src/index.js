73d58e5096ea730cccd7e9680c0b77ce38e2bedf
Eric Cote <eric@coteexpert.com>
2018-11-13 17:56:05 -0500

    Click event to increase counter (wrong, by mutating state)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//using state : initialize and show
+//using state: using events
+//modify state and see nothing on the screen
+//We can use a ReactDOM.render to re-render
+
+//We have a warning, this is not the right way.
 class Counter extends React.Component{
     constructor(props) {
         super(props);
-        this.state={count: props.init};
+        this.state={count: parseInt(props.init)};
     }
 
     render() {
         return ( 
-            <h1>
-                Counter is {this.state.count}
-            </h1>
+            <>
+                <h1>Counter is {this.state.count}</h1>
+                <button onClick={this.increase.bind(this)} >Increase</button>
+            </>
         );
     }
 
-}
+    increase() {
+        this.state.count = this.state.count + 1;
+        console.log(this.state.count);
 
+        //ReactDOM.render(<Counter init={this.state.count} />, 
+        //        document.getElementById('root'));
+    }
+}
 
 ReactDOM.render(<Counter init="1" />, 
                 document.getElementById('root'));
