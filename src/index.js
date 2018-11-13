92336e74eff1d09b1dfcea0324ca4690d97695ae
Eric Cote <eric@coteexpert.com>
2018-11-13 15:46:01 -0500

    Creating a React component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//We can put the jsx in a variable
-const hello = <h1>Hello world!</h1>
+//We can create a component using a function
+//(rule: all component must start with an uppercase letter, 
+//while DOM elements start with a lowercase)
+function Hello() {
+    return <h1>Hello world!</h1>
+} 
 
 
-ReactDOM.render(hello, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));
 
